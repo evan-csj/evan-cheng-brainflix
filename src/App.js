99cb7df5cdb859b699e2bb86f521a0ae6ee6876b
Evan Cheng <evancheng2014@gmail.com>
2023-02-03 01:15:16 -0800

    create button and search bar style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Header} from "./components/Component/Component";
+import Header from "./components/Header/Header";
 import "./App.scss";
 
 function App() {
---
+++ src/components/Header/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import logo from "../../assets/logo/BrainFlix-logo.svg";
+import search from "../../assets/icons/search.svg";
+import upload from "../../assets/icons/upload.svg";
+import avatar from "../../assets/images/Mohan-muruge.jpg";
+
+import "./Header.scss";
+
+function Header() {
+    return (
+        <header>
+            <img className="logo" src={logo} alt="Logo" />
+            <div className="search-box">
+                <input className="no-error" type="text" placeholder="Search"/>
+                <img className="icon" src={search} alt="search" />
+                <img className="avatar" src={avatar} alt="avatar" />
+            </div>
+            <div className="upload-button">
+                <button type="submit">UPLOAD</button>
+                <img className="icon" src={upload} alt="upload"/>
+            </div>
+            <img className="avatar" src={avatar} alt="avatar" />
+        </header>
+    );
+}
+
+export default Header;
