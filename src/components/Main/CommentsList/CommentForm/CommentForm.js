6aa4a28e1279088866ab55a5dd32103c77f898c3
Evan Cheng <evancheng2014@gmail.com>
2023-02-12 02:18:09 -0800

    add auto size change for text area

--- src/components/Main/CommentsList/CommentForm/CommentForm.js
+++ src/components/Main/CommentsList/CommentForm/CommentForm.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import WindowDimensions from '../../../WindowDimension';
 import addCommentIcon from '../../../../assets/icons/add_comment.svg';
 import './CommentForm.scss';
 
 function Form(props) {
+	const [textAreaRows, setTextAreaRows] = useState(1);
+	const { width } = WindowDimensions();
+
 	const submitHandler = event => {
 		event.preventDefault();
 		props.addComment(event.target.content.value);
 		event.target.reset();
+		setTextAreaRows(1);
+	};
+
+	const handleChange = event => {
+		const padding = width < 768 ? 12 : 16;
+		const lineHeight = width < 768 ? 18 : 22;
+
+		const height = event.target.scrollHeight - padding * 2;
+		const requiredRows = Math.ceil(height / lineHeight);
+		setTextAreaRows(requiredRows);
 	};
 
 	return (
@@ -14,12 +28,14 @@ function Form(props) {
 			<div className="text-input">
 				<label htmlFor="content">Join the conversation</label>
 				<textarea
+					className={textAreaRows > 1? "" : "oneline"}
 					type="textarea"
 					name="content"
 					id="content"
-					rows="5"
+					rows={textAreaRows}
 					placeholder="Add a new comment"
 					required
+					onChange={handleChange}
 				/>
 			</div>
 			<div className="button">
---
+++ src/components/WindowDimension.js
@@ -0,0 +1,9 @@
+const WindowDimensions = () => {
+	const { innerWidth: width, innerHeight: height } = window;
+	return {
+		width,
+		height,
+	};
+};
+
+export default WindowDimensions;
