8220c876ec292b35cf5fa79a0bb9af76b3848265
Evan Cheng <evancheng2014@gmail.com>
2023-02-23 02:32:18 -0800

    add upload page

--- src/pages/Upload/Upload.js
+++ src/pages/Upload/Upload.js
@@ -1,26 +1,44 @@
 import React from 'react';
+import preview from '../../assets/images/Upload-video-preview.jpg';
+import publish from '../../assets/icons/publish.svg';
+import './Upload.scss';
 
 function Upload() {
 	return (
 		<section>
 			<h1>Upload Video</h1>
-			<label>Video Thumbnail</label>
-			<label htmlFor="video-title">Title Your Video</label>
-			<input
-				className="no-error"
-				name="video-title"
-				type="text"
-				placeholder="Add a title to your video"
-			/>
-			<label htmlFor="video-description">Add a Video Description</label>
-			<textarea
-				type="textarea"
-				name="video-description"
-				rows="3"
-				placeholder="Add a new comment"
-				required
-				// onChange={handleChange}
-			/>
+			<form>
+				<div>
+					<label>Video Thumbnail</label>
+					<img src={preview} alt="preview" />
+				</div>
+				<div>
+					<label htmlFor="video-title">Title Your Video</label>
+					<input
+						className="no-error"
+						name="video-title"
+						type="text"
+						placeholder="Add a title to your video"
+					/>
+				</div>
+				<div>
+					<label htmlFor="video-description">
+						Add a Video Description
+					</label>
+					<textarea
+						type="textarea"
+						name="video-description"
+						rows="3"
+						placeholder="Add a new comment"
+						required
+						// onChange={handleChange}
+					/>
+				</div>
+				<button type="submit">
+					<span>Publish</span>
+					<img className="icon--button" src={publish} alt="publish" />
+				</button>
+			</form>
 		</section>
 	);
 }
