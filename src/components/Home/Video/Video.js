c580faae37be482fbe1fb9ef0bd3ea898cd1898a
Evan Cheng <evancheng2014@gmail.com>
2023-03-24 02:08:47 -0700

    add video controls for play and mute

--- src/components/Home/Video/Video.js
+++ src/components/Home/Video/Video.js
@@ -1,62 +1,103 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { API_ADDRESS } from '../../axios';
-import playIcon from '../../../assets/icons/play.svg';
-import pauseIcon from '../../../assets/icons/pause.svg';
-import scrubIcon from '../../../assets/icons/scrub.svg';
-import fullscreenIcon from '../../../assets/icons/fullscreen.svg';
-import closeFullscreenIcon from '../../../assets/icons/close_fullscreen.svg';
-import volumeUpIcon from '../../../assets/icons/volume_up.svg';
-import volumeOffIcon from '../../../assets/icons/volume_off.svg';
 import './Video.scss';
+// import playIcon from '../../../assets/icons/play.svg';
+// import pauseIcon from '../../../assets/icons/pause.svg';
+// import scrubIcon from '../../../assets/icons/scrub.svg';
+// import fullscreenIcon from '../../../assets/icons/fullscreen.svg';
+// import closeFullscreenIcon from '../../../assets/icons/close_fullscreen.svg';
+// import volumeUpIcon from '../../../assets/icons/volume_up.svg';
+// import volumeOffIcon from '../../../assets/icons/volume_off.svg';
+const testVideo =
+	'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4';
 
 function Video(props) {
+	const [playing, setPlaying] = useState(false);
+	const [mute, setMute] = useState(false);
+	const videoRef = useRef(null);
+
 	const videoUrl = props.video.image
 		? API_ADDRESS + '/' + props.video.image
 		: '';
+
+	const playHandler = control => {
+		if (control === 'play') {
+			videoRef.current.play();
+			setPlaying(true);
+		} else if (control === 'pause') {
+			videoRef.current.pause();
+			setPlaying(false);
+		}
+	};
+
+	const fullscreenHandler = control => {
+		if (control === 'request') {
+			videoRef.current.requestFullscreen();
+		} else if (control === 'exit') {
+			videoRef.current.exitFullscreen();
+		}
+	};
+
+	const muteHandler = control => {
+		if (control === 'mute') {
+			videoRef.current.muted = true;
+			setMute(true);
+		} else if (control === 'unmute') {
+			videoRef.current.muted = false;
+			setMute(false);
+		}
+	};
+
 	return (
 		<div className="video-container">
-			<video poster={videoUrl}>
-				<source src="" type="video/mp4" />
+			<video ref={videoRef} poster={videoUrl}>
+				<source src={testVideo} type="video/mp4" />
 				Your browser does not support the video tag.
 			</video>
 			<div className="video-controls">
-				<img
-					className="video-controls__play"
-					src={playIcon}
-					alt="play"
-					data-state="play"
-				/>
-				<img
-					className="video-controls__pause"
-					src={pauseIcon}
-					alt="pause"
-					data-state="pause"
-				/>
-				<img
-					className="video-controls__scrub"
-					src={scrubIcon}
-					alt="scrub"
-				/>
-				<img
-					className="video-controls__fullscreen"
-					src={fullscreenIcon}
-					alt="fullscreen"
-				/>
-				<img
-					className="video-controls__close-fullscreen"
-					src={closeFullscreenIcon}
-					alt="close fullscreen"
-				/>
-				<img
-					className="video-controls__volume-up"
-					src={volumeUpIcon}
-					alt="volume up"
-				/>
-				<img
-					className="video-controls__volume-off"
-					src={volumeOffIcon}
-					alt="volume off"
-				/>
+				<div className="video-controls__play-pause">
+					{playing ? (
+						<button
+							className="pause-button"
+							type="button"
+							onClick={() => playHandler('pause')}
+						></button>
+					) : (
+						<button
+							className="play-button"
+							type="button"
+							onClick={() => playHandler('play')}
+						></button>
+					)}
+				</div>
+
+				<div className="video-controls__progress">
+					<progress className="progress--video" value="20" max="100">
+						{/* <span className="bar"></span> */}
+					</progress>
+				</div>
+
+				<div className='video-controls__fullscreen-mute'>
+					<button
+						className="video-controls__fullscreen"
+						type="button"
+						onClick={() => fullscreenHandler('request')}
+					></button>
+
+					{mute ? (
+						<button
+							className="video-controls__volume-off"
+							type="button"
+							onClick={() => muteHandler('unmute')}
+						></button>
+					) : (
+						<button
+							className="video-controls__volume-up"
+							type="button"
+							onClick={() => muteHandler('mute')}
+						></button>
+					)}
+				</div>
 			</div>
 		</div>
 	);
